23473d2168264792da2ffc9835bf5ec4323bdeba
Andres Vargas Quitian <andrewquitian@gmail.com>
2021-11-13 18:05:51 -0500

    Archivo 1 de Andres con principio single listo

--- Single Responsibility.py
+++ Single Responsibility.py
@@ -1,8 +1,8 @@
-#Principio S
+#Principio S: Responsabilidad Unica.
 
-#Una clase debe encargarse de una sola cosa.
+#Esta establece que una clase, componente o microservicio debe ser responsable de una sola cosa.
 
-#Creamos la clase pacientes, cuya unica tarea es almacenar los pacientes en la base de datos.
+#Creamos la clase pacientes, cuya funcion es crear el consecutivo del numero de pacientes.
 class Pacientes():
     def __init__(self):
         self.pacientes = {}
@@ -15,7 +15,7 @@ class Pacientes():
     def __str__(self):
         return str(self.pacientes)
 
-#Creamos otra clase para el registro de los pacientes en la base de datos.
+#Creamos la clase PacientesRegistrados, cuya funcion es guardar el nombre del paciente en el archivo de texto.
 class PacientesRegistrados():
     @staticmethod
     def guardar_pacientes(filename, pacientes):
@@ -30,7 +30,9 @@ p.add_paciente("Celina Rodriguez")
 p.add_paciente("Juan Vargas")
 print(f"Pacientes registrados: {p}")
 
-#Implentacion de la clase encargada de almacenar la informaciong
+#Implentacion de la clase encargada de almacenar la informacion.
 
 g = PacientesRegistrados()
 g.guardar_pacientes("pacientes.txt", p)
+
+#Referencia: Este principio fue hecho acoplandome al enviado por el compañero Juan Carlos Monsalve G.
